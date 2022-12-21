d11ca5c2091bfdab7a2c8daf66350769e2e3eede
vishaludhan <120046348+vishaludhan@users.noreply.github.com>
2022-12-21 13:04:15 +0530

    Update HomePage.java

--- openkartproject/src/test/java/pageObject/HomePage.java
+++ openkartproject/src/test/java/pageObject/HomePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage 
 {
-	
+	//thise is mybranch code
 	@FindBy(xpath = "//span[text()='My Account']")private WebElement myAccDrpdwn;
 	@FindBy(xpath = "//a[text()='Register']") private WebElement regButton;
 	@FindBy(linkText = "Login")private WebElement loginbutton;
